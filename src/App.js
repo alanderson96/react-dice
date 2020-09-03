fb33dba16937d9c501c1c5d1cff1f0d7912affba
alanderson96 <alxanderson96@gmail.com>
2020-09-02 19:49:28 -0600

    react dice

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+import Die from "./components/Die";
+
+function App() {
+  const [numberOfSides, setNumberOfSides] = React.useState(6);
+  const [diceBag, setDiceBag] = React.useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const existingDiceBag = diceBag.splice();
+    existingDiceBag.push(numberOfSides);
+    setDiceBag(existingDiceBag);
+  }
+  return (
+    <div>
+      <h1>My React Dice Bag</h1>
+      <form onSubmit={handleSubmit}>
+        <h3>Create a new Die</h3>
+        <label>
+          Number of Sides:
+          <input
+            type="number"
+            value={numberOfSides}
+            onChange={(e) => setNumberOfSides(e.target.value)}
+          />
+        </label>
+        <button>Create</button>
+      </form>
+      <h3>Dice Bag:</h3>
+      {diceBag.map((dieSides) => {
+        {
+          return <Die numberOfSides={dieSides} />;
+        }
+      })}
+    </div>
+  );
+}
+
+export default App;
